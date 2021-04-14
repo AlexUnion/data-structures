73064bef83d519bf30ad09d33b827cf4c13e7561
Alex Kyrychenko <47228245+AlexUnion@users.noreply.github.com>
2021-04-14 14:51:12 +0300

    init: add index.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import LinkedList from "./linkedList/linkedList";
+
+const list = new LinkedList<string>();
+
+list.append("from");
+list.append("linked");
+list.prepend("Hello");
+list.append("!!!");
+//list.appendAfter("linked", "list");
+console.log(list.toArray().join(" "));
