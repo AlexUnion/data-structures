6dc6a8963f7728c844481ef11f53587ced3c4878
Al_chemick <alex2015mig@gmail.com>
2021-04-18 16:43:57 +0300

    feat: add getAll to Tree interface

--- tree/interfaces.ts
+++ tree/interfaces.ts
@@ -3,5 +3,6 @@ export interface INode {
   children: Array<INode>;
   addChild: (node: INode) => void;
   get: (key: string) => INode | undefined;
+  getAll: () => Array<INode>;
   isEmpty: () => boolean;
 }
