f70f512f185add2f0b7f30502198e74082d5a247
Al_chemick <alex2015mig@gmail.com>
2021-05-03 18:49:32 +0300

    test: add some test to "Stack" structure

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import LinkedList from "./linkedList/linkedList";
+import Stack from "./stack/stack";
 
 const list = new LinkedList<string>();
 
@@ -8,3 +9,16 @@ list.prepend("Hello");
 list.append("!!!");
 list.appendAfter("linked", "list");
 console.log(list.toArray().join(" "));
+
+const stack = new Stack<string>("hello", "world");
+
+console.log(
+  'Stack: initial state: "hello", "world" \nlength: ' + stack.getLength()
+);
+console.log("Stack: peek - " + stack.peek() + "\nlength: " + stack.getLength());
+stack.push("me");
+console.log('Stack: push - "me"\nlength: ' + stack.getLength());
+console.log("Stack: peek - " + stack.peek() + "\nlength: " + stack.getLength());
+console.log("Stack: pop - " + stack.pop() + "\nlength: " + stack.getLength());
+console.log("Stack: peek - " + stack.peek() + "\nlength: " + stack.getLength());
+console.log("Stack: peek - " + stack.peek() + "\nlength: " + stack.getLength());
